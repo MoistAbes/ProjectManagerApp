f3252e898685625ca4d72a010a2e5cd4b1942661
Sebastian Szymlowski <sebaszymek@gmail.com>
2024-01-08 21:06:09 +0100

    Added TaskRepositoryIntegrationTest class testing TaskReposutory crud functions

--- src/test/java/com/moistAbes/projectManager/TestDataUtil.java
+++ src/test/java/com/moistAbes/projectManager/TestDataUtil.java
@@ -5,6 +5,7 @@ import com.moistAbes.projectManager.domain.entity.TaskEntity;
 import org.springframework.scheduling.config.Task;
 
 import javax.xml.crypto.Data;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -29,38 +30,47 @@ public class TestDataUtil {
                 .build();
     }
 
+    public static TaskEntity createTestTaskA(){
+        return TaskEntity.builder()
+                .title("TaskTitleA")
+                .content("TaskContentA")
+                .priority("TaskPriorityA")
+                .status("TestStatusA")
+                .progress("TaskProgressA")
+                .startDate(LocalDate.of(1987, 5, 3))
+                .endDate(LocalDate.of(1987, 5, 4))
+                .build();
+    }
+
+    public static TaskEntity createTestTaskB(){
+        return TaskEntity.builder()
+                .title("TaskTitleB")
+                .content("TaskContentB")
+                .priority("TaskPriorityB")
+                .status("TestStatusB")
+                .progress("TaskProgressB")
+                .startDate(LocalDate.of(2001, 2, 12))
+                .endDate(LocalDate.of(2003, 2, 25))
+                .build();
+    }
+
+    public static TaskEntity createTestTaskC(){
+        return TaskEntity.builder()
+                .title("TaskTitleC")
+                .content("TaskContentC")
+                .priority("TaskPriorityC")
+                .status("TestStatusC")
+                .progress("TaskProgressC")
+                .startDate(LocalDate.of(1995, 11, 5))
+                .endDate(LocalDate.of(1995, 12, 16))
+                .build();
+    }
+
     public static List<TaskEntity> createTestTaskListA(){
-
         return List.of(
-                TaskEntity.builder()
-                        .title("TestTaskTitleA")
-                        .content("TestTaskContentA")
-                        .status("TestTaskStatusA")
-                        .priority("TestTaskPriorityA")
-                        .progress("TestTaskProgressA")
-                        .startDate(new Date())
-                        .endDate(new Date())
-                        .build()
-
-                ,TaskEntity.builder()
-                        .title("TestTaskTitleB")
-                        .content("TestTaskContentB")
-                        .status("TestTaskStatusB")
-                        .priority("TestTaskPriorityB")
-                        .progress("TestTaskProgressB")
-                        .startDate(new Date())
-                        .endDate(new Date())
-                        .build()
-
-                ,TaskEntity.builder()
-                        .title("TestTaskTitleC")
-                        .content("TestTaskContentC")
-                        .status("TestTaskStatusC")
-                        .priority("TestTaskPriorityC")
-                        .progress("TestTaskProgressC")
-                        .startDate(new Date())
-                        .endDate(new Date())
-                        .build()
+                createTestTaskA(),
+                createTestTaskB(),
+                createTestTaskC()
         );
     }
 }
